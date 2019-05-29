59354c01fc97deac1a7e6fa0c57ab80dbf5830cc
shack254 <123meshack@gmail.com>
2019-05-29 09:27:00 +0300

    add program to show that a triangle is equilateral

---
+++ java script/scripts.js
@@ -0,0 +1,8 @@
+var ab = parseInt(prompt("enter number"));
+var bc = parseInt(prompt("enter number"));
+var ca = parseInt(prompt("enter number"));
+if(ab===bc && bc===ca && ab===ca){
+    alert("triangle is eqilateral");
+}else if( ab==bc || bc===ca || ab==ca){
+    alert("triangle is isosceles");
+}else if((ab+bc>ca || ab + ca > bc || ca + bc >ab) && (ab!=bc && bc!=ca && ab!=ca))
