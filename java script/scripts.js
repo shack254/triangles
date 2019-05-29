2697dd21f86a52b427577d9f8ea36026731dcf19
shack254 <123meshack@gmail.com>
2019-05-29 10:16:08 +0300

    add program to show that a triangle is scalene

--- java script/scripts.js
+++ java script/scripts.js
@@ -3,10 +3,10 @@ var bc = parseInt(prompt("enter number"));
 var ca = parseInt(prompt("enter number"));
 if(ab===bc && bc===ca && ab===ca){
     alert("triangle is eqilateral");
-}else if( ab==bc || bc===ca || ab==ca){
+}else if( (ab==bc || bc===ca || ab==ca) && (!(ab+bc<=ca || ab+ca<=bc || ca+bc<=ab))){
     alert("triangle is isosceles");
-}else if((ab+bc>ca || ab + ca > bc || ca + bc >ab) && (ab!=bc && bc!=ca && ab!=ca)){
-    alert("scalene triangle")
-}else if (ab+bc <= ac || ab+ca <=bc || ac+bc<=ab){
+}else if((ab+bc>ca || ab+ca>bc || ca+bc>ab) && (ab!=bc && bc!=ca && ab!=ca) && (!(ab+bc<=ca || ab+ca<=bc || ca+bc<=ab))){
+    alert("scalene triangle");
+}else{
     alert("a triangle cannot be formed");
 }
