bccefdef43ee8200960925c909f37236958cf585
shack254 <123meshack@gmail.com>
2019-05-29 14:08:00 +0300

    fixed form bug

--- java script/scripts.js
+++ java script/scripts.js
@@ -1,14 +1,14 @@
 function triangle(){
-var ab = parseInt(document.getElementById("sideab"));
-var bc = parseInt(document.getElementById("sidebc"));
-var ca = parseInt(document.getElementById("sideca"));
+var ab = parseInt(document.getElementById("sideab").value);
+var bc = parseInt(document.getElementById("sidebc").value);
+var ca = parseInt(document.getElementById("sideca").value);
 if(ab===bc && bc===ca && ab===ca){
     alert("triangle is eqilateral");
-}else if( (ab===bc || bc===ca || ab==ca) && (!(ab+bc<=ca || ab+ca<=bc || ca+bc<=ab))){
+}else if(ab==bc || bc===ca || ab==ca){
     alert("triangle is isosceles");
-}else if((ab+bc>ca || ab+ca>bc || ca+bc>ab) && (ab!=bc && bc!=ca && ab!=ca) && (!(ab+bc<=ca || ab+ca<=bc || ca+bc<=ab))){
+}else if((!(ab+bc<=ca || ab+ca<=bc || ca+bc<=ab)) && (!(ab===bc && bc===ca && ab===ca)) ){
     alert("scalene triangle");
-}else{
+}else if  (ab+bc<=ca || ab+ca<=bc || ca+bc<=ab){
     alert("a triangle cannot be formed");
 }
 }
