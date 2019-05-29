7e79c00d7567903e0214a039b96702b14c957fea
shack254 <123meshack@gmail.com>
2019-05-29 12:01:50 +0300

    add a form to input the variables

--- java script/scripts.js
+++ java script/scripts.js
@@ -1,12 +1,15 @@
-var ab = parseInt(prompt("enter number"));
-var bc = parseInt(prompt("enter number"));
-var ca = parseInt(prompt("enter number"));
+function triangle(){
+var ab = parseInt(document.getElementById("sideab"));
+var bc = parseInt(document.getElementById("sidebc"));
+var ca = parseInt(document.getElementById("sideca"));
 if(ab===bc && bc===ca && ab===ca){
     alert("triangle is eqilateral");
-}else if( (ab==bc || bc===ca || ab==ca) && (!(ab+bc<=ca || ab+ca<=bc || ca+bc<=ab))){
+}else if( (ab===bc || bc===ca || ab==ca) && (!(ab+bc<=ca || ab+ca<=bc || ca+bc<=ab))){
     alert("triangle is isosceles");
 }else if((ab+bc>ca || ab+ca>bc || ca+bc>ab) && (ab!=bc && bc!=ca && ab!=ca) && (!(ab+bc<=ca || ab+ca<=bc || ca+bc<=ab))){
     alert("scalene triangle");
 }else{
     alert("a triangle cannot be formed");
 }
+}
+
