524cc6e520d32c992fae212f66e31ae4b606a7db
shack254 <123meshack@gmail.com>
2019-05-29 18:17:33 +0300

    addedan output

--- java script/scripts.js
+++ java script/scripts.js
@@ -3,13 +3,13 @@ var ab = parseInt(document.getElementById("sideab").value);
 var bc = parseInt(document.getElementById("sidebc").value);
 var ca = parseInt(document.getElementById("sideca").value);
 if(ab===bc && bc===ca && ab===ca){
-    alert("triangle is eqilateral");
-}else if(ab==bc || bc===ca || ab==ca){
-    alert("triangle is isosceles");
-}else if((!(ab+bc<=ca || ab+ca<=bc || ca+bc<=ab)) && (!(ab===bc && bc===ca && ab===ca)) ){
-    alert("scalene triangle");
+    Text= "this is a equlateral triangle";
+}else if(ab===bc || bc===ca || ab===ca){
+    Text= "triangle is isosceles";
+}else if((!(ab+bc>ca || ab+ca<=bc || ca+bc<=ab)) && (!(ab===bc && bc===ca && ab===ca)) ){
+    Text="scalene triangle";
 }else if  (ab+bc<=ca || ab+ca<=bc || ca+bc<=ab){
-    alert("a triangle cannot be formed");
+    Text="a triangle cannot be formed";
 }
+document.getElementById("demo").innerHTML = Text;
 }
-
