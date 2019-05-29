910984f5a4e57044c2e5a7615fba7eeb48f5171f
shack254 <123meshack@gmail.com>
2019-05-29 09:49:53 +0300

    add program to show that a triangle the values entered cannot from a triangle

--- java script/scripts.js
+++ java script/scripts.js
@@ -7,4 +7,6 @@ if(ab===bc && bc===ca && ab===ca){
     alert("triangle is isosceles");
 }else if((ab+bc>ca || ab + ca > bc || ca + bc >ab) && (ab!=bc && bc!=ca && ab!=ca)){
     alert("scalene triangle")
+}else if (ab+bc <= ac || ab+ca <=bc || ac+bc<=ab){
+    alert("a triangle cannot be formed");
 }
