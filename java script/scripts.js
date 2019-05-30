f697b821d302a156833b363c2d85fdf1cd1daf27
shack254 <123meshack@gmail.com>
2019-05-30 12:43:18 +0300

    added all outputs tio images

--- java script/scripts.js
+++ java script/scripts.js
@@ -11,24 +11,24 @@ if(ab===bc && bc===ca && ab===ca){
     document.body.appendChild(x);
     Text= "triangle is equilateral";
 }else if(ab===bc || bc===ca || ab===ca){
-    x.setAttribute("src", "img_pulpit.jpg");
+    x.setAttribute("src", "images/images.png");
     x.setAttribute("width", "304");
     x.setAttribute("height", "228");
     x.setAttribute("alt", "The Pulpit Rock");
     document.body.appendChild(x);
     Text= "triangle is isosceles";
 }else if((!(ab+bc>ca || ab+ca<=bc || ca+bc<=ab)) && (!(ab===bc && bc===ca && ab===ca)) ){
-    x.setAttribute("src", "img_pulpit.jpg");
+    x.setAttribute("src", "images/scalen.png");
     x.setAttribute("width", "304");
     x.setAttribute("height", "228");
     x.setAttribute("alt", "The Pulpit Rock");
     document.body.appendChild(x);
     Text="scalene triangle";
 }else if  (ab+bc<=ca || ab+ca<=bc || ca+bc<=ab){
-    x.setAttribute("src", "img_pulpit.jpg");
+    x.setAttribute("src", "images/no.jpeg");
     x.setAttribute("width", "304");
     x.setAttribute("height", "228");
-    x.setAttribute("alt", "The Pulpit Rock");
+    x.setAttribute("alt", "no traingle");
     document.body.appendChild(x);
     Text="a triangle cannot be formed";
 }
