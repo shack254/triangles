3a81e5caf398d8220f1fc2cbb83f1ae11c13aed3
shack254 <123meshack@gmail.com>
2019-05-30 13:07:08 +0300

    added added texts

--- java script/scripts.js
+++ java script/scripts.js
@@ -2,34 +2,34 @@ function triangle(){
 var ab = parseInt(document.getElementById("sideab").value);
 var bc = parseInt(document.getElementById("sidebc").value);
 var ca = parseInt(document.getElementById("sideca").value);
-var x = document.createElement("IMG");
+var IMG = document.createElement("IMG");
 if(ab===bc && bc===ca && ab===ca){
-    x.setAttribute("src", "images/equilateral.png");
-    x.setAttribute("width", "304");
-    x.setAttribute("height", "228");
-    x.setAttribute("alt", "The Pulpit Rock");
-    document.body.appendChild(x);
+    IMG.setAttribute("src", "images/equilateral.png");
+    IMG.setAttribute("width", "304");
+    IMG.setAttribute("height", "228");
+    IMG.setAttribute("alt", "The Pulpit Rock");
+    document.body.appendChild(IMG);
     Text= "triangle is equilateral";
 }else if(ab===bc || bc===ca || ab===ca){
-    x.setAttribute("src", "images/images.png");
-    x.setAttribute("width", "304");
-    x.setAttribute("height", "228");
-    x.setAttribute("alt", "The Pulpit Rock");
-    document.body.appendChild(x);
+    IMG.setAttribute("src", "images/images.png");
+    IMG.setAttribute("width", "304");
+    IMG.setAttribute("height", "228");
+    IMG.setAttribute("alt", "The Pulpit Rock");
+    document.body.appendChild(IMG);
     Text= "triangle is isosceles";
 }else if((!(ab+bc>ca || ab+ca<=bc || ca+bc<=ab)) && (!(ab===bc && bc===ca && ab===ca)) ){
-    x.setAttribute("src", "images/scalen.png");
-    x.setAttribute("width", "304");
-    x.setAttribute("height", "228");
-    x.setAttribute("alt", "The Pulpit Rock");
-    document.body.appendChild(x);
+    IMG.setAttribute("src", "images/scalen.png");
+    IMG.setAttribute("width", "304");
+    IMG.setAttribute("height", "228");
+    IMG.setAttribute("alt", "The Pulpit Rock");
+    document.body.appendChild(IMG);
     Text="scalene triangle";
 }else if  (ab+bc<=ca || ab+ca<=bc || ca+bc<=ab){
-    x.setAttribute("src", "images/no.jpeg");
-    x.setAttribute("width", "304");
-    x.setAttribute("height", "228");
-    x.setAttribute("alt", "no traingle");
-    document.body.appendChild(x);
+    IMG.setAttribute("src", "images/no.jpeg");
+    IMG.setAttribute("width", "304");
+    IMG.setAttribute("height", "228");
+    IMG.setAttribute("alt", "no traingle");
+    document.body.appendChild(IMG);
     Text="a triangle cannot be formed";
 }
 document.getElementById("demo").innerHTML = Text;
