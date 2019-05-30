40bb17e937f999de91596f113f70508fe754c6a4
shack254 <123meshack@gmail.com>
2019-05-30 11:15:36 +0300

    output as image

--- java script/scripts.js
+++ java script/scripts.js
@@ -2,13 +2,34 @@ function triangle(){
 var ab = parseInt(document.getElementById("sideab").value);
 var bc = parseInt(document.getElementById("sidebc").value);
 var ca = parseInt(document.getElementById("sideca").value);
+var x = document.createElement("IMG");
 if(ab===bc && bc===ca && ab===ca){
-    Text= "this is a equlateral triangle";
+    x.setAttribute("src", "images/equilateral.png");
+    x.setAttribute("width", "304");
+    x.setAttribute("height", "228");
+    x.setAttribute("alt", "The Pulpit Rock");
+    document.body.appendChild(x);
+    Text= "triangle is equilateral";
 }else if(ab===bc || bc===ca || ab===ca){
+    x.setAttribute("src", "img_pulpit.jpg");
+    x.setAttribute("width", "304");
+    x.setAttribute("height", "228");
+    x.setAttribute("alt", "The Pulpit Rock");
+    document.body.appendChild(x);
     Text= "triangle is isosceles";
 }else if((!(ab+bc>ca || ab+ca<=bc || ca+bc<=ab)) && (!(ab===bc && bc===ca && ab===ca)) ){
+    x.setAttribute("src", "img_pulpit.jpg");
+    x.setAttribute("width", "304");
+    x.setAttribute("height", "228");
+    x.setAttribute("alt", "The Pulpit Rock");
+    document.body.appendChild(x);
     Text="scalene triangle";
 }else if  (ab+bc<=ca || ab+ca<=bc || ca+bc<=ab){
+    x.setAttribute("src", "img_pulpit.jpg");
+    x.setAttribute("width", "304");
+    x.setAttribute("height", "228");
+    x.setAttribute("alt", "The Pulpit Rock");
+    document.body.appendChild(x);
     Text="a triangle cannot be formed";
 }
 document.getElementById("demo").innerHTML = Text;
