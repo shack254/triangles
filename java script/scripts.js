67b8f07679dcf42bb389d9cf570de0e833800da6
shack254 <123meshack@gmail.com>
2019-05-31 17:57:22 +0300

    changed fornt

--- java script/scripts.js
+++ java script/scripts.js
@@ -5,7 +5,7 @@ var ca = parseInt(document.getElementById("sideca").value);
 if  ((ab+bc<=ca || ab+ca<=bc || ca+bc<=ab) || (ab<=0 || bc<=0 || ca<=0)){
     Text="a triangle cannot be formed";
 }else if(ab===bc && bc===ca && ab===ca){
-    Text= "triangle is equilateral";
+    Text= "triangle is equilateral ";
 }else if(ab===bc || bc===ca || ab===ca){
     Text= "triangle is isosceles";
 }else if((!(ab+bc>ca || ab+ca<=bc || ca+bc<=ab)) && (!(ab===bc && bc===ca && ab===ca)) ){
