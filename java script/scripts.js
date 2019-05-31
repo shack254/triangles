be72d055f41fffb4a27a3a0912917fbc9f5d9167
shack254 <123meshack@gmail.com>
2019-05-31 16:04:15 +0300

    add a lable for each output

--- java script/scripts.js
+++ java script/scripts.js
@@ -2,10 +2,10 @@ function triangle(){
 var ab = parseInt(document.getElementById("sideab").value);
 var bc = parseInt(document.getElementById("sidebc").value);
 var ca = parseInt(document.getElementById("sideca").value);
-if(ab===bc && bc===ca && ab===ca){
-    Text= "triangle is equilateral";
-}else if  (ab+bc<=ca || ab+ca<=bc || ca+bc<=ab){
+if  ((ab+bc<=ca || ab+ca<=bc || ca+bc<=ab) || (ab<=0 || bc<=0 || ca<=0)){
     Text="a triangle cannot be formed";
+}else if(ab===bc && bc===ca && ab===ca){
+    Text= "triangle is equilateral";
 }else if(ab===bc || bc===ca || ab===ca){
     Text= "triangle is isosceles";
 }else if((!(ab+bc>ca || ab+ca<=bc || ca+bc<=ab)) && (!(ab===bc && bc===ca && ab===ca)) ){
